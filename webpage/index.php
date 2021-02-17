a188e104d37648a4c55a0d23d8156c67527d2ddc
nanzerrous <nanzerrous7997@gmail.com>
2021-02-18 00:32:37 +0700

    Trucorp second commit fix

--- webpage/index.php
+++ webpage/index.php
@@ -33,7 +33,7 @@
             echo "0 result";
         }
         echo "<br>";
-        echo "Total jumlah user yang ada di database : " . $count . " user" ;
+        echo "Total jumlah user yang ada di database : " . $count . " user(s)" ;
         $conn->close();
     ?>
 
